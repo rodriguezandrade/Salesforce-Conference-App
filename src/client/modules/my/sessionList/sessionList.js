23e9e8af6bcc912259a922d749429bcaeea1efe6
Jonathan Andrade <jearsoft@gmail.com>
2019-10-27 17:16:44 -0600

    Create the sessionDetails Web Component

--- src/client/modules/my/sessionList/sessionList.js
+++ src/client/modules/my/sessionList/sessionList.js
@@ -1,17 +1,25 @@
 import { LightningElement, track } from 'lwc';
 import { getSessions } from 'data/sessionService';
 export default class SessionList extends LightningElement {
-  @track sessions = [];
-  connectedCallback() {
-    getSessions().then(result => {
-      this.sessions = this.allSessions = result;
-    });
-  }
+    @track sessions = [];
+    connectedCallback() {
+        getSessions().then(result => {
+            this.sessions = this.allSessions = result;
+        });
+    }
 
-  handleSearchKeyInput(event) {
-    const searchKey = event.target.value.toLowerCase();
-    this.sessions = this.allSessions.filter(
-      session => session.name.toLowerCase().includes(searchKey)
-    );
-  }
+    handleSearchKeyInput(event) {
+        const searchKey = event.target.value.toLowerCase();
+        this.sessions = this.allSessions.filter(session =>
+            session.name.toLowerCase().includes(searchKey)
+        );
+    }
+    
+    handleSessionClick(event) {
+        const index = event.currentTarget.dataset.index;
+        const navigateEvent = new CustomEvent('navigate', {
+          detail: this.sessions[index].id
+        });
+        this.dispatchEvent(navigateEvent);
+      }
 }
